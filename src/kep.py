d252ac506cad9ceae557166b44aed8353c53ca42
= <jcloward@yc.edu>
2019-10-24 17:26:22 -0700

    Add File class for storing and parsing specific file information

--- src/kep.py
+++ src/kep.py
@@ -1,13 +1,54 @@
 # Module imports
 from sys import argv as args
+from datetime import datetime
 
 # Local imports
 
+# Parsing objects0
+class file:
+    _date_format = "%Y-%m-%d %H:%M:%S"
 
-# Parsing objects
+    def __init__(title,
+                 date_created=None,
+                 date_modified=None,
+                 tags=[],
+                 files=[]):
+        self.title = title
+        self.date_created = date_created
+        self.date_modified = date_modified
+        self.tags = tags
+        self.files = files
+
+    def get_default_header(self):
+        header = ""
+
+        if self.title:
+            header += f"Title: {title}\n"
+
+        if self.date_created is not None:
+            d = date_created.strftime(self._date_format)
+            header += f"Date Created: {d}\n"
+
+        if self.date_modified is not None:
+            d = date_modified.strftime(self._date_format)
+            header += f"Date Modified: {d}\n"
+
+        if len(self.tags) > 0:
+            t = " ".join(self.tags)
+            header += f"Tags: {t}\n"
+
+        if len(self.files) > 0:
+            t = " ".join(self.files)
+            header += f"Files: {t}\n"
+
+        header += "-----\n\n"
+
+        return header
+
+
 class Kep:
     @classmethod
-    def list(self, subfolder = "./"):
+    def list(self, subfolder="./"):
         pass
 
     @classmethod
@@ -15,9 +56,9 @@ class Kep:
         pass
 
     @classmethod
-    def read(self, location, assetname = null):
-        if assetname == null:
-            assetname = location.split('/').[-1]
+    def read(self, location, assetname=None):
+        if assetname is None:
+            assetname = location.split('/')[-1]
             location = location.split('/')[:-1].join('/')
 
 
@@ -30,7 +71,7 @@ if __name__ == '__main__':
 
     # Single argument tasks
     if len(args) == 2:
-        if args[1].lower() == "list"
+        if args[1].lower() == "list":
             Kep.list()
 
         #...
